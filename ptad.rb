7e9dcbcd28a2c04c2fca91ebe5f9fc1b17c40d8e
Takayuki Nakata <f.seasons017@gmail.com>
2018-12-18 09:43:37 +0900

    Update ptad.rb

--- ptad.rb
+++ ptad.rb
@@ -4,7 +4,7 @@ HOMEBREW_PTAD_VERSION='0.0.1'
 class Ptad < Formula
   desc "Papertrail Archives Downloader"
   homepage "https://github.com/giraffate/ptad"
-  url "https://github.com/giraffate/ptad/releases/download/v#{HOMEBREW_PTAD_VERSION}/ptad_darwin_amd64"
+  url "https://github.com/giraffate/ptad/releases/download/v#{HOMEBREW_PTAD_VERSION}/ptad_v#{HOMEBREW_PTAD_VERSION}_darwin_amd64.zip"
   sha256 "e6a901c96f38b2049616d966b50e9811dfedc057d86483ad39d891a480a30f61"
 
   def install
